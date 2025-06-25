e2a1558d6f755cb408b495430c344a1c8a8fcf1e
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2025-06-25 17:40:25 -0400

    cleanup.

--- pup-finance-server/server.js
+++ pup-finance-server/server.js
@@ -9,4 +9,6 @@ const server = http.createServer(app);
 
 server.listen(PORT, () => {
     console.log(`Server is running: http://localhost:${PORT}`);
+    console.log(`AUTH0 Domain: ${process.env.AUTH0_DOMAIN}`)
+    console.log(`AUTH0 Audience: ${process.env.AUTH0_AUDIENCE}`)
 })
