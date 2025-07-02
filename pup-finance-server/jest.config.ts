b2a097e8f6abed0df81aa469e16214f27616ab09
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2025-07-02 16:33:33 -0400

    changed config and made into ts

--- pup-finance-server/jest.config.ts
+++ pup-finance-server/jest.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     // Automatically clear mock calls and instances between every test:
@@ -8,5 +10,13 @@ module.exports = {
     // A list of the paths to directories that Jest should use to search for files in.
     roots: ['<rootDir>/src'],
     // The test match pattern
-    testMatch: ['**/__tests__/**/*.test.ts'],
+    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+    moduleNameMapper:{
+        // This line is the crucial part.
+    // It tells Jest to map any import starting with '@/'
+    // to the '<rootDir>/src/' directory.
+    '^@/(.*)$': '<rootDir>/src/$1',
+    }
 }
+
+export default config;
